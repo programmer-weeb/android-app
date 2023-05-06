6dddfbcef774f764866c6cff42ae110667364851
AhmedWindows11 <forprogramming8@gmail.com>
2023-05-06 18:20:49 +0300

    Make a notification when click login btn

--- app/src/main/java/com/example/edu/login.java
+++ app/src/main/java/com/example/edu/login.java
@@ -3,22 +3,29 @@ package com.example.edu;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "my_channel";
+
     private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        createNotificationChannel();
         loginButton = (Button) findViewById(R.id.button2);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,14 +33,37 @@ public class login extends AppCompatActivity {
                 // fromLoginToHome
                 Intent intent = new Intent(login.this, home.class);
                 startActivity(intent);
-
+//                notification
+                sendNotification();
             }
-           
         });
     }
+    private void sendNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("Login occurred")
+                        .setContentText("You have logged in successfully!");
 
+        Notification notification = builder.build();
 
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
     public void goToSignUpPage(View view) {
         Intent intent = new Intent(this, signup.class);
         startActivity(intent);
