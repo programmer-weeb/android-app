f96ece2dc214a57ea257f88c0258f1b93fd8248e
AhmedWindows11 <forprogramming8@gmail.com>
2023-05-06 14:12:07 +0300

    remove shopping btn

--- app/src/main/java/com/example/edu/home.java
+++ app/src/main/java/com/example/edu/home.java
@@ -2,7 +2,9 @@ package com.example.edu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void open_profile(View view) {
+        Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
     }
 }
