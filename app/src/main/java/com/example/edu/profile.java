ba293c8c908bae08f17218ad59130e0f9643a9f2
AhmedWindows11 <forprogramming8@gmail.com>
2023-05-06 22:27:06 +0300

    make Implicit intent to open maps

--- app/src/main/java/com/example/edu/profile.java
+++ app/src/main/java/com/example/edu/profile.java
@@ -3,6 +3,7 @@ package com.example.edu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,5 +18,11 @@ public class profile extends AppCompatActivity {
     public void open_profile(View view) {
         Intent intent = new Intent(this, profile.class);
         startActivity(intent);
+    }
+
+    public void openMap(View v) {
+        Intent location = new Intent(Intent.ACTION_VIEW);
+        location.setData(Uri.parse("https://maps.app.goo.gl/pjP1Tjz91963Sq6N8"));
+        startActivity(location);
     }
 }
