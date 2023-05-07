91e1f4f2e9c45f0731ffd3f3484db3dcbb4e6529
AhmedWindows11 <forprogramming8@gmail.com>
2023-05-07 10:01:42 +0300

    new splash (better)

--- app/src/main/java/com/example/edu/MainActivity.java
+++ app/src/main/java/com/example/edu/MainActivity.java
@@ -13,7 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, home.class));
+                finish();
+            }
+        }, 3000);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -21,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        }, 1000);
+        }, 3000);
     }
 }
